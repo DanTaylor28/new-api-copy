000566ef4e88efcb27bfbbd9b63e1af88413ec6c
DanTaylor28 <dan.taylor1493@gmail.com>
2023-03-04 14:01:59 +0000

    Create post model and ensure it works as expected

---
+++ posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+# from categories.models import Category
+
+
+class Post(models.Model):
+    """
+    The Post model which contains all relevant fields for any posts created.
+    A default image is attached in the case of a user not uploading one
+    themselves.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True)
+    caption = models.TextField(blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # category = models.ForeignKey(
+    #     Category, on_delete=models.RESTRICT, null=True, blank=True)
+    post_image = models.ImageField(
+        upload_to='images/', default='../default_post_l03unw', blank=True
+    )
+
+    class Meta:
+        ordering = ['-uploaded_at']
+
+    def __str__(self):
+        return f"{self.title} | owner: {self.owner}"
---
+++ posts/serializers.py
@@ -0,0 +1,64 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Post
+# from pins.models import Pin
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Nemrous extra fields are present to show num_of_pins/
+    comments, category name and more. Image validation is also
+    present ensuring the file size, height and width doesn't
+    exceed the maximum.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(
+        source='owner.profile.profile_image.url')
+    is_post_owner = serializers.SerializerMethodField()
+    # pinned_id = serializers.SerializerMethodField()
+    # category_name = serializers.ReadOnlyField(source='category.name')
+    # num_of_pins = serializers.ReadOnlyField()
+    # num_of_comments = serializers.ReadOnlyField()
+    updated_at = serializers.SerializerMethodField()
+
+    # def validate_post_image(self, value):
+    #     if value.size > 1024 * 1024 * 2:
+    #         raise serializers.ValidationError(
+    #             'Image files cannot be larger than 2mb'
+    #         )
+    #     if value.image.width > 4096:
+    #         raise serializers.ValidationError(
+    #             'Image width cannot be larger than 4096px'
+    #         )
+    #     if value.image.height > 4096:
+    #         raise serializers.ValidationError(
+    #             'Image height cannot be larger than 4096px'
+    #         )
+    #     return value
+
+    def get_is_post_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    # def get_pinned_id(self, obj):
+    #     user = self.context['request'].user
+    #     if user.is_authenticated:
+    #         pin = Pin.objects.filter(
+    #             owner=user, post=obj
+    #         ).first()
+    #         return pin.id if pin else None
+    #     return None
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'title', 'caption', 'owner', 'is_post_owner',
+            #  'category', 'category_name',
+            'uploaded_at', 'updated_at',
+            'post_image', 'profile_id', 'profile_image',  # 'pinned_id',
+            # 'num_of_pins', 'num_of_comments'
+        ]
